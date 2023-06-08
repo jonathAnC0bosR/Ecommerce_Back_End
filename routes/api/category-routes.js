4b801b8a1dcf34925eebc4b57c78951e271bb636
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-06-08 09:42:08 -0600

    Product-routes update method now it works and the relations in the index now are fully functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,28 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+//Why it is not working?
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+
+  console.log("Request Params: ", req.params)
+  console.log("Request Body: ", req.body)
+  // let newData = req.body;
+
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: { id: req.params.id }
+    });
+
+    if(!updateCategory) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    }
+    res.status(200).json(updateCategory);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
