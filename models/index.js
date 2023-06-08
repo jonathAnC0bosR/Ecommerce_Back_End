74014edcb7864a195cebf5d5ce2248b1c658e2f6
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-06-07 18:21:55 -0600

    Routes from category-routes now work except put

--- models/index.js
+++ models/index.js
@@ -7,7 +7,10 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
